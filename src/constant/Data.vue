324d3e6a919bb4408a8f66591a77269029712509
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2023-10-28 12:03:15 +0100

    reduce the percentage of the hard Code

---
+++ src/constant/Data.vue
@@ -0,0 +1,13 @@
+<script>
+export const statistics = [
+    {state:'1k+',name:"Brands"},
+    {state:'250+',name:"Shops"},
+    {state:'250k+',name:"Customers"}
+];
+export const Nav =[
+    {name:"Home",hre :"#Home"},
+    {name:"About",hre :"#About"},
+    {name:"Product",hre :"#Product"},
+    {name:"Contact",hre :"#Contact"},
+]
+</script>
--- src/section/Home.vue
+++ src/section/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="#Home" class="mt-10 ml-32 grid grid-cols-2 grid-flow-col	 relative" >
+    <div id="#Home" class="mt-10 ml-32 grid grid-cols-2 grid-flow-col" >
         <div class="flex flex-col space-y-7">
             <p class=" text-Orange-0 font-Montserrat text-base  ">Our Summer Collection</p>
             <h1 class="  whitespace-nowrap capitalize text-8xl font-bold font-Palanquin z-10">The new arrival <br>  <span class=" whitespace-nowrap text-Orange-0 font-Palanquin">nike</span> shoes</h1>
@@ -12,25 +12,17 @@
             <path fill-rule="evenodd" clip-rule="evenodd" d="M11.0886 6.18499C10.8717 6.43163 10.8717 6.83153 11.0886 7.07817L13.1032 9.36842H5.55556C5.24873 9.36842 5 9.65119 5 10C5 10.3488 5.24873 10.6316 5.55556 10.6316H13.1032L11.0886 12.9218C10.8717 13.1685 10.8717 13.5684 11.0886 13.815C11.3056 14.0617 11.6574 14.0617 11.8743 13.815L14.8373 10.4466C15.0542 10.1999 15.0542 9.80005 14.8373 9.55341L11.8743 6.18499C11.6574 5.93834 11.3056 5.93834 11.0886 6.18499Z" fill="#FF6452"/>
             </svg>
             </button>
-            <div class="flex space-x-10  ">
-                <div class=" ">
-                    <h2 class=" font-Palanquin text-5xl">1k+</h2>
-                    <p class=" font-Montserrat text-Gray-0 ">Brands</p>
-                </div>
-                <div>
-                    <h2 class=" font-Palanquin text-5xl">250+</h2>
-                    <p class=" font-Montserrat text-Gray-0 ">Shops</p>
-                </div>
-                <div>
-                    <h2 class=" font-Palanquin text-5xl">250k+</h2>
-                    <p class=" font-Montserrat text-Gray-0 ">Customers</p>
+            <div class="flex space-x-10">
+                <div class="" v-for="S in statistics">
+                    <h2 class="font-Palanquin text-5xl">{{ S.state }}</h2>
+                    <p class="font-Montserrat text-Gray-0">{{ S.name }}</p>
                 </div>
             </div>
         </div>
         <div class=" col-start-2">
-            <svg class=" z-0  bottom-7" width="610" height="700" viewBox="0 0 506 505" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class=" z-0 h-screen   " width="610" viewBox="0 0 506 505" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="505.626" height="502.15" rx="30" fill="#C1C4DE"/>
-                <mask id="mask0_2_359" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="506" height="505">
+                <mask id="mask0_2_359" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0">
                 <rect width="505.626" height="504.485" rx="30" fill="#C1C4DE"/>
                 </mask>
                 <g mask="url(#mask0_2_359)">
@@ -39,7 +31,10 @@
                 <path d="M460.564 -87.5859C419.278 12.0524 366.347 98.4086 311.258 192.884C304.359 204.716 295.955 216.161 289.523 228.151C279.368 247.081 272.253 276.119 274.875 296.552C281.003 344.301 344.953 386.251 392.056 408.746C425.306 424.626 473.312 454.081 510.307 464.195" stroke="white" stroke-opacity="0.8" stroke-width="3" stroke-linecap="round"/>
                 </g>
             </svg>
-            <img class="z-50" src="../assets/Pictures/hero-26fc9f87.png" alt="">
+
         </div>
     </div>
 </template>
+<script setup>
+import { statistics } from '../constant/Data.vue';
+</script>
